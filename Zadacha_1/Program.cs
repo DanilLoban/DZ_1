d2cc679e77d44f8d91160d7572261eb3348ad573
Danil <dan.loban@yandex.ru>
2022-12-01 18:33:18 +0300

    first1

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число, ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число, ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write(num1);
+}
+else
+{
+    Console.Write(num2);
+}
